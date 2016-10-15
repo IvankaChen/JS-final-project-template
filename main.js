0905e209418c94cf79c2af6fcb43baa0284361ed
IvankaChen <Ivanka.chen@gmail.com>
2016-10-15 17:15:40 +0800

    person1
    person1

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ bgImg.src = "images/map.png";
 function draw(){
   ctx.drawImage(bgImg,0,0);
 }
-
-
   setInterval(draw,16);
+
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/jason.gif";
+
+function person1(){
+  ctx.drawImage(enemyImg,0,0);
+}
+
+  setInterval(person1,16);
