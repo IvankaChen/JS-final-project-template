1bb982781a322d3448264f90aa867426530dcd33
IvankaChen <Ivanka.chen@gmail.com>
2016-10-15 17:19:39 +0800

    刪減行數
    刪減行數

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
-
 function draw(){
   ctx.drawImage(bgImg,0,0);
 }
@@ -12,19 +10,14 @@ function draw(){
 
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/jason.gif";
-
 function person1(){
   ctx.drawImage(enemyImg,0,0);
 }
-
   setInterval(person1,16);
-
 
 var enemy2Img = document.createElement("img");
 enemy2Img.src = "images/rukia.gif";
-
 function person2(){
   ctx.drawImage(enemy2Img,0,0);
 }
-
   setInterval(person2,16);
