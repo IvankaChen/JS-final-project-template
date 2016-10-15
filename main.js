dc6852b447769be609b863c6217ea74d14118e17
IvankaChen <Ivanka.chen@gmail.com>
2016-10-15 16:45:33 +0800

    輸入程式碼
    輸入程式碼

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-var bglmg = document.createElement("img");
-bglmg.src = "images/map.png";
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
 var canvas = document.getElementByld("game-canvas");
 var ctx = canvas.getContext("2d");
+
+function draw(){
+ctx.drawImage(bgImg,0,0);
+}
+
+draw();
