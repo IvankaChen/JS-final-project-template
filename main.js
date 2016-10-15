2afb153ce8b84bb70c692f58ef9e686d40af690f
IvankaChen <Ivanka.chen@gmail.com>
2016-10-15 17:03:05 +0800

    順序改
    順序改

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var bgImg = document.createElement("img");
-bgImg.src = "images/map.png";
+
 var canvas = document.getElementByld("game-canvas");
 var ctx = canvas.getContext("2d");
 
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+
 function draw(){
-ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(bgImg,0,0);
 }
 
 
-setInterval(draw,16);
+  setInterval(draw,16);
