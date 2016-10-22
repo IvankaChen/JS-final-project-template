b5ebce62514bece8e3621a3bd7a283a1085915c8
IvankaChen <Ivanka.chen@gmail.com>
2016-10-22 16:29:40 +0800

    btn
    btn

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function person3(){
 var button = document.createElement("img");
 button.src = "images/tower-btn.png";
 function buttonf(){
-  ctx.drawImage(button,200,200);
+  ctx.drawImage(button,500,250);
 }
   setInterval(buttonf,16);
 
