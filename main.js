9ff51a64bae6c7b67560a1814d22f37753677fba
IvankaChen <Ivanka.chen@gmail.com>
2016-10-29 15:21:13 +0800

    towerImg-button

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ enemy2Img.src = "images/rukia.gif";
 var enemy3Img = document.createElement("img");
 enemy3Img.src = "images/daigh.gif";
 var button = document.createElement("img");
-towerImg.src = "images/tower-btn.png";
+button.src = "images/tower-btn.png";
 var tower = document.createElement("img");
 tower.src = "images/tower.png";
 
@@ -27,7 +27,7 @@ ctx.drawImage(bgImg,0,0);
 ctx.drawImage(enemyImg,enemyImg1.x,enemyImg1.y);
 ctx.drawImage(enemy2Img,95,100);
 ctx.drawImage(enemy3Img,0,0);
-ctx.drawImage(towerImg,580,420,60,60);  
+ctx.drawImage(button,580,420,60,60);  
 ctx.drawImage(tower,cursor.x,cursor.y);
 }
 setInterval(draw1,16);
