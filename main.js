4de8fbb4dda664bbfc87c02fb12dc5e555169160
IvankaChen <Ivanka.chen@gmail.com>
2016-10-29 15:16:08 +0800

    31

--- main.js
+++ main.js
@@ -29,9 +29,9 @@ ctx.drawImage(enemy2Img,95,100);
 ctx.drawImage(enemy3Img,0,0);
 ctx.drawImage(button,580,420,60,60);
    if(isBuilding){
-  ctx.drawImage(towerbuiltImg,cursor.x,cursor.y);
+  ctx.drawImage(tower,cursor.x,cursor.y);
   }
-ctx.drawImage(tower,cursor.x,cursor.y);
+ctx.drawImage(towerbuilding,cursor.x,cursor.y);
 }
 setInterval(draw1,16);
 
