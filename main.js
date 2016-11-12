aba92202de185fd89d5d225536559df59f65b8d2
IvankaChen <Ivanka.chen@gmail.com>
2016-11-12 17:51:03 +0800

    move

--- main.js
+++ main.js
@@ -9,11 +9,39 @@ function Enemy(){
   this.y = 480-32;
   this.speedX = 0;
   this.speedY = -64;
+  this.pathDes = 0;
+  this.speed = 64;
   this.move = function(){
-  this.x = this.x + this.speedX/FPS;
-  this.y = this.y + this.speedY/FPS;
+        if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/FPS, this.speed/FPS) ){
+
+            // 首先，移動到下一個路徑點
+            this.x = enemyPath[this.pathDes].x;
+            this.y = enemyPath[this.pathDes].y;
+
+            // 指定下一個路徑點
+            this.pathDes++;
+
+            // 重新設定設定前往目標路徑點的所需的水平/垂直速度
+            if (enemyPath[this.pathDes].x>this.x) {
+              this.speedX = 64;
+              this.speedY = 0;
+            } else if (enemyPath[this.pathDes].x<this.x) {
+              this.speedX = -64;
+              this.speedY = 0;
+            } else if (enemyPath[this.pathDes].y>this.y) {
+              this.speedX = 0;
+              this.speedY = 64;
+            } else if (enemyPath[this.pathDes].y<this.y) {
+              this.speedX = 0;
+              this.speedY = -64;
+            }
+
+        } else {
+            this.x = this.x + this.speedX/FPS;
+            this.y = this.y + this.speedY/FPS;
+        }
+    };
 }
-};
 var enemy = new Enemy();
 
 //找圖片
@@ -31,6 +59,21 @@ var towerbuiltImg = document.createElement("img");
 towerbuiltImg.src = "images/tower.png";
 var slimeImg = document.createElement("img");
 slimeImg.src = "images/slime.gif";
+
+//敵人路徑
+var enemyPath = [
+{x:64, y:92},
+{x:224,y:92},
+{x:224,y:220},
+{x:320,y:220},
+{x:320,y:314},
+{x:192,y:314},
+{x:192,y:378},
+{x:416,y:378},
+{x:416,y:314},
+{x:512,y:314},
+{x:512,y:90}
+];
 
 //畫畫
 function draw1(){
